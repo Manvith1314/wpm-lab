4fdc3a88d650d6e6300abbf8f24cff611ddc3305
sri-sathvika19 <22eg107c65@anurag.edu.in>
2024-08-05 14:14:42 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express varshh' });
+  res.render('index', { title: 'A19 Express' });
 });
 
 module.exports = router;
